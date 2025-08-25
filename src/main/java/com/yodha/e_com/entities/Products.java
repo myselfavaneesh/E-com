1f6ff51c5787ad689039a4aa8a3a50ca4dce32c2
myselfavaneesh <avaneeshjaiswal2k25@gmail.com>
2025-08-26 01:18:19 +0530

    Day 2

--- src/main/java/com/yodha/e_com/entities/Products.java
+++ src/main/java/com/yodha/e_com/entities/Products.java
@@ -1,14 +1,10 @@
 package com.yodha.e_com.entities;
 
-import jdk.jfr.Category;
 import lombok.Data;
-import lombok.NonNull;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.util.ArrayList;
 import java.util.List;
 
 @Document(collection = "products")
